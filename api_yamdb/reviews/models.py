947d67921d3689c43bae868eb68a03f960fc41bf
Yuriy Smirnov <sielents@yandex.ru>
2024-06-17 20:33:57 +0300

    StartSprint

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название категории')
+    slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название жанра')
+    slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name = 'жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    year = models.IntegerField(verbose_name='Год выпуска')
+    description = models.CharField(verbose_name='Описание')
+    genre = models.ManyToManyField(
+        Genre, through='GenreTitle', verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name = 'произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
