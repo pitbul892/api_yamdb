882e1d0542daf6256017bbe4e0fc48a50809b353
Andrey Anisimov <qwertfy@yandex.ru>
2024-06-18 12:45:04 +0300

    Добавление docstrings.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,9 @@
+"""Обновленная модель пользователя."""
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class MyUser(AbstractUser):
+    """Модель пользователя с дополнительнвым полем."""
     is_moderator = models.BooleanField('Модератор', default=False)
