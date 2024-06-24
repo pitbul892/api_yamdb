3d0c26df4fe1c69b13c35743cd922132c0515095
pitbul892 <153751347+pitbul892@users.noreply.github.com>
2024-06-24 20:40:03 +0300

    Revert "Feature/titles categories genres (#7)"
    This reverts commit cb933ae4d3fe498767753a0728dcb2b97565948f.

--- api_yamdb/api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-
-
-class IsAdminOrReadOnly(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS or (
-            request.user.is_authenticated and request.user.is_admin()
-        )
