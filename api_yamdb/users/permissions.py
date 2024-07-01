fedc87337398ab4658345009a460f43abad18546
pitbul892 <153751347+pitbul892@users.noreply.github.com>
2024-07-01 22:26:44 +0300

    Update permissions.py

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import permissions, status
-from rest_framework.response import Response
+from rest_framework import permissions
 
 User = get_user_model()
 
