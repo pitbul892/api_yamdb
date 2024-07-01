00906516c991a0b93c8b073662e305624b6f4d74
Andrey Anisimov <qwertfy@ya.ru>
2024-07-01 22:03:53 +0300

    .

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, generics, permissions, status
+from rest_framework import filters, permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.decorators import action
 from rest_framework.response import Response
